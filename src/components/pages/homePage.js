26b58d952ef689f4ac3e6235a7d0963a13cc7085
Gino <giancarlo.buenaflor@yahoo.com>
2018-01-17 19:36:11 +0100

    added semantic-ui and tested basic logic

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import {Button, Icon} from 'semantic-ui-react'
+import {Button, Icon, Form} from 'semantic-ui-react'
+import TextArea from 'semantic-ui-react/dist/commonjs/addons/TextArea/TextArea';
 class Homepage extends Component {
-  handleClick = () => {
-    alert("woow");
+  constructor(props){
+    super(props)
+    this.state = {
+      textValue: '',
+    }
   }
+
+
+  onClick = (event) => {
+    var str = this.state.textValue;
+    alert(str.replace("Microsoft", "Microsoft got replaced"))
+  }
+
   render() {
     return (
        <div className="container-fluid">
@@ -13,17 +24,14 @@ class Homepage extends Component {
         <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
         </p>
-
-        <textarea>
-           Hello there, this is some text in a text area
-        </textarea>
-
-        <Button>Hi</Button>
-
-        <button onClick={this.handleClick}>
-           Click me
-        </button>
-      
+        <Button onClick={this.handleClick}>Hi</Button>
+        
+        <Form>
+        <Form.TextArea onChange={(e) => this.setState({textValue: e.target.value})}
+              value={this.state.textValue} label='About' placeholder='Füge deinen Text hier ein...' />
+        <Form.Checkbox label='I stimme dem Blabal zu' />
+        <Form.Button onClick={this.onClick}>Text Gendern!</Form.Button>
+        </Form>
        </div>
     );
   }
